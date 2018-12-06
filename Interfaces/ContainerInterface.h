d3d31289c8d4376c6b7d58a50ab3c032272fdfbc
Andrew Nicoara <ldnic2013@gmail.com>
2018-12-05 21:51:58 -0600

    Update ContainerInterface.h

--- Interfaces/ContainerInterface.h
+++ Interfaces/ContainerInterface.h
@@ -4,7 +4,9 @@
 template<class ItemType>
 class ContainerInterface
 {
-  // Insert Code Here
+ public:
+  virtual void push_back(const ItemType & item);
+  virtual void pop_back(void);
 };
 
 #endif
